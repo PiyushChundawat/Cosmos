a0997c23b760e99f350a3377b9384432fe3ce27f
PiyushChundawat <piyush06.jee2023@gmail.com>
2026-03-15 15:15:47 +0530

    Remove trailing slash from baseURL in axios config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cosmos-7k46.onrender.com/',
+  baseURL: 'https://cosmos-7k46.onrender.com',
 });
 
 // Add token to every request
