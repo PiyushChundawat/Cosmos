06585beeb3b324b9e05fdda2df74559bf87f49eb
PiyushChundawat <piyush06.jee2023@gmail.com>
2026-03-15 15:32:18 +0530

    Fix import path for Sidebar component

--- frontend/src/pages/StudentAnalytics.jsx
+++ frontend/src/pages/StudentAnalytics.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Sidebar from '../components/Sidebar';
+import Sidebar from '../components/SideBar';
 import Card from '../components/Card';
 import Chart from '../components/Chart';
 import Input from '../components/Input';
